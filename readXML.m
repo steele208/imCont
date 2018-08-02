66540589b04cea8c60745922d31eeb0006c2b6b2
Lachlan  Steele <s4358007@uq.edu.au>
2018-08-02 14:38:15 +1000

    Commenting and speeding for readXML

--- readXML.m
+++ readXML.m
@@ -1,33 +1,28 @@
 function output = readXML()
-[xf, xD] = uigetfile('.xml');
-tic;
-tree = xmlread(strcat(xD, xf));
-childNode = tree.getChildNodes;
+[xf, xD] = uigetfile('.xml'); % Find file
+tic; % timing 
+tree = xmlread(strcat(xD, xf)); % Read file
+childNode = tree.getChildNodes; % First set of children
+output = struct('Name', [], 'Data', []); % Initialise output structure
 
-childIdx = 1;
-imNode = childNode.getFirstChild.item(childIdx);
-while ~isempty(imNode)
-    if ~strcmp(imNode.getTagName, 'Images')
-        childIdx = childIdx + 2;
-        imNode = childNode.getFirstChild.item(childIdx);
-    else
-        break;
-    end
-end
-output = struct('Name', [], 'Data', []);
+%% Cycle through first level children to find 'Images'
+    
+% 'Images' Tag is always the second last
+imNode = childNode.getFirstChild.getLastChild.getPreviousSibling;
+
 for idx = 1 : 2 : imNode.getLength - 1
-    % ceil(idx/2)
+    % ceil(idx/2) will recover normal counting from '#text' node skipping
     infoNode = imNode.item(idx).getChildNodes; % get specific image detail
     n = 1;
     node = infoNode.item(n); % get singular detail
-    while ~isempty(node)
+    while ~isempty(node) % loop through all details
         name = char(node.getNodeName); 
         if strcmp(name, 'URL')
             output(ceil(idx/2)).Name = char(node.getTextContent);
         end
         output(ceil(idx/2)).Data.(name) = char(node.getTextContent);
-        n = n + 2;
-        node = infoNode.item(n);  
+        n = n + 2; % jump in 2's to avoid '#text'
+        node = infoNode.item(n); % next item
     end
 end
 toc;
