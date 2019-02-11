998c1ae2d0924eaabb511c651958bce17781fe23
Lachlan  Steele <s4358007@uq.edu.au>
2019-02-12 08:49:06 +1000

    Major difference to path detection. Including the running average path. Needs more bug fixing, not always getting max length for avgPath.

--- path_detection.m
+++ path_detection.m
@@ -1,23 +1,52 @@
 function userData = path_detection(userData)
 for set = 1 : length(userData.tracked)
     skipped = [];
-    for prtcl = 1 : numel(userData.tracked{1,1})
-        %% Per particle
+    for prtcl = 1 : numel(userData.tracked{set,1})
+        % Per particle
+        userData.tracked{set,1}(prtcl).AbsTime(1) = 0;
         userData.tracked{set,1}(prtcl).Displacement(1) = 0;
         userData.tracked{set,1}(prtcl).Position(1,3) = 0;
+        userData.tracked{set,1}(1).AvgPath = ...
+            userData.tracked{set,1}(1).Position(:,3);
         if length(userData.tracked{set,1}(prtcl).Time) < 2
             skipped(end + 1) = prtcl; %#ok<AGROW>
             continue
         end
         for pth = 2 : size(userData.tracked{set,1}(prtcl).Position,1)
-        d = sqrt(...
-            (userData.tracked{set,1}(prtcl).Position(pth,1) - userData.tracked{set,1}(prtcl).Position(pth-1,1))^2 ...
-          + (userData.tracked{set,1}(prtcl).Position(pth,2) - userData.tracked{set,1}(prtcl).Position(pth-1,2))^2);
-        userData.tracked{set,1}(prtcl).Position(pth,3) = d + userData.tracked{set,1}(prtcl).Position(pth-1,3);
-        userData.tracked{set,1}(prtcl).Displacement(1) = d + userData.tracked{set,1}(prtcl).Displacement(1);
-        userData.tracked{set,1}(prtcl).FinalTime(1) = userData.tracked{set,1}(prtcl).Time(end);
+        % find squared difference / path movement
+            d = sqrt(...
+                (userData.tracked{set,1}(prtcl).Position(pth,1) - ...
+                userData.tracked{set,1}(prtcl).Position(pth-1,1))^2 ...
+              + (userData.tracked{set,1}(prtcl).Position(pth,2) - ...
+              userData.tracked{set,1}(prtcl).Position(pth-1,2))^2);
+        % Absolute path length
+            userData.tracked{set,1}(prtcl).Position(pth,3) = ...
+                d + userData.tracked{set,1}(prtcl).Position(pth-1,3);
+        % Sum of displacement
+            userData.tracked{set,1}(prtcl).Displacement(1) = ...
+                d + userData.tracked{set,1}(prtcl).Displacement(1);
+        % time as absolute
+            userData.tracked{set,1}(prtcl).AbsTime(pth) = ...
+                userData.tracked{set,1}(prtcl).Time(pth) - ...
+                userData.tracked{set,1}(prtcl).Time(1); 
+        % pad master path for mean path calculation
+            if length(userData.tracked{set,1}(prtcl).Time) < ...
+                    length(userData.tracked{set,1}(1).AvgPath)
+                pad = length(userData.tracked{set,1}(1).Time);
+                userData.tracked{set,1}(prtcl).Position(pad,3) = 0;
+            elseif length(userData.tracked{set,1}(prtcl).Time) > ...
+                    length(userData.tracked{set,1}(1).AvgPath)
+                pad = length(userData.tracked{set,1}(prtcl).Time);
+                userData.tracked{set,1}(1).AvgPath(pad,1) = 0;
+            end
+            userData.tracked{set,1}(1).AvgPath = ...
+                userData.tracked{set,1}(1).AvgPath + ...
+                userData.tracked{set,1}(prtcl).Position(:,3);
         end
+        % division for avg path
+        userData.tracked{set,1}(1).AvgPath = ...
+            userData.tracked{set,1}(1).AvgPath ./ ...
+            size(userData.tracked{set,1}(prtcl).Position,1);
     end
     userData.tracked{set,1}(skipped) = [];
 end
-pause(1);
