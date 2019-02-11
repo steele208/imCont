4729ccc7e8db4f6a24504000fddf448569b09058
Lachlan  Steele <s4358007@uq.edu.au>
2019-02-12 08:50:14 +1000

    Graph result initial commit. Most graphing working. Consider multiple subplots having same axis limmits.

---
+++ graph_results.m
@@ -0,0 +1,30 @@
+function graph_results(userData)
+%{
+% Needs:
+%   - Mean/nanmean per point
+%   - Mean per set subplot/single axis
+%}
+
+
+% Assuming that all images are at the same resolution:
+res = userData.metaData(1).Data.ImageResolutionX;
+res = str2double(res);
+
+for set = 1 : length(userData.tracked)
+    figure(set)
+    
+    for pth = 1 : length(userData.tracked{set,1})
+        subplot(2, 1, 1);
+        hold on
+        plot(userData.tracked{set,1}(pth).AbsTime,...
+            res.*userData.tracked{set,1}(pth).Position...
+            (1:length(userData.tracked{set,1}(pth).AbsTime),3));
+        ylabel('Distance Travelled (m)');
+        xlabel('Time (ms)');
+        title('Particle Displacement Due To Brownian Motion');
+    end
+    subplot(2, 1, 2);
+    hold on
+    plot(userData.tracked{set,1}(1).AbsTime,...
+        res.*userData.tracked{set,1}(1).AvgPath);
+end
