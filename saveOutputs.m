9986123a5af87a31e7e63f775979c423b2315176
Lachlan  Steele <s4358007@uq.edu.au>
2019-02-13 09:26:34 +1000

    Saving outputs includes .png of figures, accessed through figure pointers. To avoid large data files, figure pointers are deleted before data is saved`

--- saveOutputs.m
+++ saveOutputs.m
@@ -2,7 +2,9 @@ function saveOutputs(userData)
 curFolder = pwd;
 msgStem = 'Select Location to Save ';
 msg{3} = 'Successfully Saved to:';
-
+%Enums
+DATA = 1;
+GRAPH = 2;
 if userData.save
     doSave = 1;
 else 
@@ -13,24 +15,24 @@ while(doSave == 1)
     % Cancelling the save from the dialog box will cause a loop and the
     % save request to be asked again; if saving isn't selected then the 
     % while loop won't enter
-        % images
-        if userData.save == 3 || userData.save == 2
-            msg{1} = strcat(msgStem, ' Graphs');
-            hdl = makeDialog(msg{1});
-            [file, path] = uiputfile('*.mat', 'Save Data to File');
-            disp(file);
-            saveErr = makeSave(userData, path, file); % Error, might be ignored 
-        end
-        
+
         % Data 
         if userData.save == 3 || userData.save == 1
             cd(curFolder);
             msg{1} = strcat(msgStem, ' Data');
             hdl = makeDialog(msg{1});
             [file, path] = uiputfile('*.mat', 'Save Data to File');
-            saveErr = makeSave(userData, path, file); % Error, might be ignored 
+            saveErr = makeSave(DATA, userData, path, file); % Error, might be ignored 
         end
         
+        % images
+        if userData.save == 3 || userData.save == 2
+            msg{1} = strcat(msgStem, ' Graphs');
+            hdl = makeDialog(msg{1});
+            [file, path] = uiputfile('*.png', 'Save Graph to File');
+            disp(file);
+            saveErr = makeSave(GRAPH, userData, path, file); % Error, might be ignored 
+        end
         if ~saveErr
             msg{4} = strcat(path, file);
             uicontrol('Parent', hdl, 'Style', 'text',...
@@ -45,8 +47,14 @@ if exist('hdl', 'var')
     delete(hdl);
 end
    
-function errorFlag = makeSave(uRhe_Out, path, file)
-    if any(~file) || any(~path) || isempty(uRhe_Out)
+function errorFlag = makeSave(TYPE, uRhe_Out, path, file)
+    if isempty(uRhe_Out) || ~isfield(uRhe_Out, 'figure')
+        warndlg('No data found for save','WARNING');
+        errorFlag = 2;
+        return
+    end
+   
+    if any(~file) || any(~path)
         answer = questdlg('Save Failed, Try again?');
         switch answer 
             case 'Yes'
@@ -57,8 +65,26 @@ function errorFlag = makeSave(uRhe_Out, path, file)
                 errorFlag = 2;
         end
     else
-        save(strcat(path,file), 'uRhe_Out', '-v7.3');
-        errorFlag = 0;
+        switch TYPE
+            case 1
+                uRhe = uRhe_Out;
+                uRhe.figure = [];
+                save(strcat(path,file), 'uRhe', '-v7.3');
+                errorFlag = 0;
+            case 2
+                for fig = 1 : numel(uRhe_Out.figure)
+                    figLoc = uRhe_Out.figure{fig};
+                    filename = strcat(path,file);
+                    if fig > 1
+                        % append filename
+                        filename = insertBefore(filename...
+                            ,'.png',strcat('_',num2str(fig)));
+                    end
+                    print(figLoc, filename, '-dpng')
+                end   
+                errorFlag = 0;
+        end 
+        
     end
     
 function hdl = makeDialog(msg)
