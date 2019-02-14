2e52d87095e4e7f5bee39ca2b318b75a84c65f1e
Lachlan  Steele <s4358007@uq.edu.au>
2019-02-15 09:54:22 +1000

    assignMeta functionalised to removed repeated datetime concerns.

--- assignMeta.m
+++ assignMeta.m
@@ -13,44 +13,37 @@ function [images] = assignMeta(handles)
         metaIdx = ceil(strfind([meta.Name], images(imIdx).Name) / strSize);
         images(imIdx).Meta = meta(metaIdx).Data;
         
-        if imIdx == 1 || images(imIdx).Set ~= setNum
-            setNum = images(imIdx).Set;
-            setIdx = imIdx;
-            t0 = images(setIdx).Meta.AbsTime;
-            % has 3 digits of millisec
-            if strcmp(t0(end-9), '.')
-                t0 = datetime(t0(1:end-6), 'InputFormat',...
-                    'yyyy-MM-dd''T''HH:mm:ss.SSS');
-            % has less than 3 digits of millisec
-            elseif contains(t0, '.')
-                tZone = find(t0 == '+');
-                mSec = find(t0 == '.');
-                t0 = t0(1:mSec+(tZone - mSec));
-                t0(end:23) = '0';
-                t0 = datetime(t0, 'InputFormat',...
-                    'yyyy-MM-dd''T''HH:mm:ss.SSS');
-            % has no millisec
-            else
-                t0(end-5:end-2) = '.000';
-                t0 = datetime(t0(1:end-2), 'InputFormat',...
-                    'yyyy-MM-dd''T''HH:mm:ss.SSS');
-            end
-            t0.Format = 'hh:mm:ss.SSS';
-        end
-
-        if imIdx == setIdx
+        t = dTime(imIdx, images);
+        
+        if imIdx == 1 || images(imIdx).Set ~= images(imIdx-1).Set
+            t0 = t;
             images(imIdx).Meta.RelTime = 0;
         else
-            imTime = images(imIdx).Meta.AbsTime(1:end-6);
-            
-            if ~contains(imTime, '.')
-            imTime = strcat(imTime,'.000');
-            end
-            
-            imTime = datetime(imTime, 'InputFormat',...
-                'yyyy-MM-dd''T''HH:mm:ss.SSS');
-            imTime.Format = 'hh:mm:ss.SSS';
-            relTime = second(imTime) - second(t0);
+            imTime = t;
+            relTime = 60*(minute(imTime) - minute(t0)) +...
+                (second(imTime) - second(t0));
             images(imIdx).Meta.RelTime = relTime;
         end
     end
+    
+function t = dTime(setIdx, images)
+        t = images(setIdx).Meta.AbsTime;
+        % has 3 digits of millisec
+        if strcmp(t(end-9), '.')
+            t = datetime(t(1:end-6), 'InputFormat',...
+                'yyyy-MM-dd''T''HH:mm:ss.SSS');
+        % has less than 3 digits of millisec
+        elseif contains(t, '.')
+            tZone = find(t == '+');
+            mSec = find(t == '.');
+            t = t(1:mSec+(tZone - mSec));
+            t(end:23) = '0';
+            t = datetime(t, 'InputFormat',...
+                'yyyy-MM-dd''T''HH:mm:ss.SSS');
+        % has no millisec
+        else
+            t(end-5:end-2) = '.000';
+            t = datetime(t(1:end-2), 'InputFormat',...
+                'yyyy-MM-dd''T''HH:mm:ss.SSS');
+        end
+        t.Format = 'hh:mm:ss.SSS';
