5c9fdb88146bb70b12f3c8c3060c0bfaa08f97fd
Lachlan  Steele <s4358007@uq.edu.au>
2019-02-14 16:11:59 +1000

    Formatting and posting messages to GUI instead of popup

--- saveOutputs.m
+++ saveOutputs.m
@@ -1,51 +1,46 @@
-function saveOutputs(userData)
-curFolder = pwd;
-msgStem = 'Select Location to Save ';
-msg{3} = 'Successfully Saved to:';
-%Enums
-DATA = 1;
-GRAPH = 2;
-if userData.save
-    doSave = 1;
-else 
-    doSave = 0;
-end
+function saveOutputs(handles)
+    userData = handles.output.UserData;
+    curFolder = pwd;
+    msgStem = 'Select Location to Save ';
+    msg{3} = 'Successfully Saved to:';
+    %Enums
+    DATA = 1;
+    GRAPH = 2;
+    if userData.save
+        doSave = 1;
+    else 
+        doSave = 0;
+    end
 
-while(doSave == 1)
-    % Cancelling the save from the dialog box will cause a loop and the
-    % save request to be asked again; if saving isn't selected then the 
-    % while loop won't enter
-
+    while(doSave == 1)
+        % Cancelling the save from the dialog box will cause a loop and the
+        % save request to be asked again; if saving isn't selected then the 
+        % while loop won't re-enter
         % Data 
         if userData.save == 3 || userData.save == 1
             cd(curFolder);
             msg{1} = strcat(msgStem, ' Data');
-            hdl = makeDialog(msg{1});
+            handles.text20.String = msg{1};
             [file, path] = uiputfile('*.mat', 'Save Data to File');
             saveErr = makeSave(DATA, userData, path, file); % Error, might be ignored 
         end
-        
+
         % images
         if userData.save == 3 || userData.save == 2
             msg{1} = strcat(msgStem, ' Graphs');
-            hdl = makeDialog(msg{1});
+            handles.text20.String = msg{1};
             [file, path] = uiputfile('*.png', 'Save Graph to File');
             disp(file);
             saveErr = makeSave(GRAPH, userData, path, file); % Error, might be ignored 
         end
         if ~saveErr
             msg{4} = strcat(path, file);
-            uicontrol('Parent', hdl, 'Style', 'text',...
-                'Position',[0 -5 300 90],...
-                'String', msg);
+            handles.text20.String = msg;
             doSave = 0;
         elseif saveErr == 2
             doSave = 0;
         end
-end
-if exist('hdl', 'var')
-    delete(hdl);
-end
+    end
    
 function errorFlag = makeSave(TYPE, uRhe_Out, path, file)
     if isempty(uRhe_Out) || ~isfield(uRhe_Out, 'figure')
@@ -83,12 +78,5 @@ function errorFlag = makeSave(TYPE, uRhe_Out, path, file)
                     print(figLoc, filename, '-dpng')
                 end   
                 errorFlag = 0;
-        end 
-        
+        end  
     end
-    
-function hdl = makeDialog(msg)
-hdl = dialog('Position', [0 500 300 100], 'Name', 'Saving');
-uicontrol('Parent', hdl, 'Style', 'text',...
-    'Position',[0 -5 300 90],...
-    'String', msg);
