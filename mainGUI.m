638f1c47bcf3d0610402f6e7955342565eb2aa57
Lachlan  Steele <s4358007@uq.edu.au>
2019-02-21 10:47:54 +1000

    Inserted particle size input. Needs to be encorporated for g*,g',g'' calculations

--- mainGUI.m
+++ mainGUI.m
@@ -22,7 +22,7 @@ function varargout = mainGUI(varargin)
 
 % Edit the above text to modify the response to help mainGUI
 
-% Last Modified by GUIDE v2.5 12-Feb-2019 12:16:37
+% Last Modified by GUIDE v2.5 21-Feb-2019 09:22:33
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -60,7 +60,7 @@ handles.output = hObject;
 
 handles.wbOA = waitbar2a(0, handles.uipanel12,'BarColor','green');
 handles.wbCur = waitbar2a(0, handles.uipanel13,'BarColor','blue');
-
+handles.text22.String = [char(956), 'm'];
 % Update handles structure
 guidata(hObject, handles);
 
@@ -538,3 +538,26 @@ function radiobutton13_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of radiobutton13
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit2 as text
+%        str2double(get(hObject,'String')) returns contents of edit2 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
