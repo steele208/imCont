e77522411029d34ea9ff4f313eea2d8883628085
Lachlan  Steele <s4358007@uq.edu.au>
2019-04-04 12:43:26 +1000

    graphing of G' G'', not perfect.

--- graph_results.m
+++ graph_results.m
@@ -5,12 +5,19 @@ function userData = graph_results(handles)
    - Mean per set subplot/single axis
 %}
 userData = handles.output.UserData;
+msg = 'Producing graphs (';
 switch handles.radiobutton12.Value
     case 0 % Don't display graphs
         vis = 'off';
+        msg = strcat(msg, 'not visible)');
     case 1
         vis = 'on';
+        msg = strcat(msg, 'visible)');
 end
+
+
+
+handles.text20.String = msg;
 % Assuming that all images are at the same resolution:
 res = str2double(userData.metaData(1).Data.ImageResolutionX);
 userData.figure = [];
@@ -19,6 +26,7 @@ for set = 1 : length(userData.tracked)
     userData.figure{end+1} = figure(set+1);
     userData.figure{end}.Visible = vis;
     
+    %{
     for pth = 1 : length(userData.tracked{set,1})
         subplot(2, 1, 1);
         hold on
@@ -30,11 +38,12 @@ for set = 1 : length(userData.tracked)
         title({'Particle Displacement Due To Brownian Motion',...
             strcat(' - Set: ', num2str(set))});
     end
-    subplot(2, 1, 2);
+    
     hold on
+    %}
     for i = 1 : length(userData.tracked{set,1})
         if all(size(userData.tracked{set,1}(1).AvgTime(:,i)) ==...
-                size(nanmean(userData.tracked{set,1}(1).AvgPath,2))) &&...
+                size(nanmean(userData.tracked{set,1}(1).AvgPath2,2))) &&...
                 ~any(isnan(userData.tracked{set,1}(1).AvgTime(:,i)))
             t = i;
             
@@ -42,8 +51,16 @@ for set = 1 : length(userData.tracked)
         end
     end
  
+    subplot(2, 1, 1);
     plot(userData.tracked{set,1}(1).AvgTime(:,t),...
-        (res.*nanmean(userData.tracked{set,1}(1).AvgPath,2)).^2);
+        userData.tracked{set,1}(1).G_Prime);
+    hold on;
+    plot(userData.tracked{set,1}(1).AvgTime(:,t),...
+        userData.tracked{set,1}(1).G_DblPrime);
+    
+    subplot(2, 1, 2);
+    plot(userData.tracked{set,1}(1).AvgTime(:,t),...
+        (res.*nanmean(userData.tracked{set,1}(1).AvgPath2,2)).^2);
     ylabel('Distance Travelled (m)');
     xlabel('Time (ms)');
     title('Mean Squared Particle Displacement');
